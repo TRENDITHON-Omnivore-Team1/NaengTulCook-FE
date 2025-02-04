afecd0132d9dc062a0f0b7d6bfe5a358b5e8c206
seunghyeonKang <number42isuser@gmail.com>
2025-02-04 21:50:26 +0900

    🔧: 절대 경로 설정

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+
+  // 경로 자동 설정
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
