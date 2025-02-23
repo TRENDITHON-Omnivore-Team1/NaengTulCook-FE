620f88a0a62dbac72e8ee1a1a70c5a5b3f966a9d
chahyunwoo00 <ckgusdn8@naver.com>
2025-02-24 01:40:10 +0900

    ✨: 이웃레시피 ui

--- src/components/community/NeighborRecipe.jsx
+++ src/components/community/NeighborRecipe.jsx
@@ -1,7 +1,98 @@
+import * as S from './NeighborRecipe.style'
+import star_red from '@/assets/community/star_red.svg'
+
+const dummy =[
+  {
+    url:'https://www.homecuisine.co.kr/files/attach/images/140/583/005/0f63b96014f8ab404e13bf16e2347c92.JPG',
+    name:'간장계란밥',
+  },
+  {
+    url:'https://i.namu.wiki/i/VanLYhMvqv8-eIzfLZ7-JrfKG58ylFqCArNPzVTLdTa05LLittzg0yfUHVhmn8YPiOknwUhwi0kJ-fGVH-m_wg.webp',
+    name:'스팸마요',
+  },
+  {
+    url:'https://i.namu.wiki/i/mG__ekAYrVaJYQh6uBRZDDBHWDn6VDaXIOg3Wi8q-cs4FE42OqkFY95bkx5lb-4KbZkS1EvKSH3Jct8GSpbyWw.webp',
+    name:'토스트',
+  },
+  {
+    url:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTpXLlJPuwYXvNgtqnhTa3-9lHoyR8m5GwQsw&s',
+    name:'알리오올리오'
+  }
+]
 export default function NeighborRecipe(){
   return(
     <>
-      <div>이웃레시피</div>
+      <S.FlexContainer>
+        <S.FlexItem>
+          <S.TitleText>같은 나이대의 사람들에게 인기에요!</S.TitleText>
+          <S.CardWrapper>
+            {dummy.map((d,index)=>{
+              return(
+                <S.CardContainer key={index}>
+                  <S.CardImage src={d.url} $width={119} $height={194}/>
+                  <S.CardText>{d.name}</S.CardText>
+                </S.CardContainer>
+              )
+            })}
+          </S.CardWrapper>
+        </S.FlexItem>
+        <S.FlexItem>
+          <S.TitleText>1인 가구에게 인기레시피</S.TitleText>
+          <S.CardWrapper>
+            {dummy.map((d,index)=>{
+              return(
+                <S.CardContainer key={index}>
+                  <S.CardImage src={d.url} $width={119} $height={107}/>
+                  <S.CardText>{d.name}</S.CardText>
+                </S.CardContainer>
+              )
+            })}
+          </S.CardWrapper>
+        </S.FlexItem>
+        <S.FlexItem>
+          <S.TextFlex>
+            <S.TitleText>프리미엄 레시피</S.TitleText>
+            <S.SubText>이웃이 등록한 유료 레시피에요</S.SubText>
+          </S.TextFlex>
+          <S.CardWrapper>
+            {dummy.map((d,index)=>{
+              return(
+                <S.CardContainer key={index}>
+                  <S.CardImage src={d.url} $width={119} $height={107}/>
+                  <S.CardText>{d.name}</S.CardText>
+                </S.CardContainer>
+              )
+            })}
+          </S.CardWrapper>
+        </S.FlexItem>
+        <div>
+          <S.TextFlex>
+            <S.TitleText>자유롭게 레시피를 공유해보세요!</S.TitleText>
+            <S.SubText2>레시피 추가하기</S.SubText2>
+          </S.TextFlex>
+          <S.ColFlexContainer>
+            <S.ColFlexItem>
+              <S.RoundImg src={dummy[0].url} />
+              <div>
+                <S.NickText>홍길동</S.NickText>
+                <S.RecipeTitle>마라+크림 조합이 의외로 환상적! 매콤한 마라 크림 우동 레시피</S.RecipeTitle>
+                <img src={star_red} style={{width:'10px',height:'10px'}}/>
+                <S.StarText>304</S.StarText>
+              </div>
+            </S.ColFlexItem>
+            <S.ColFlexItem>
+              <S.RoundImg src={dummy[0].url} />
+              <div>
+                <S.NickText>홍길동</S.NickText>
+                <S.RecipeTitle>마라+크림 조합이 의외로 환상적! 매콤한 마라 크림 우동 레시피</S.RecipeTitle>
+                <img src={star_red} style={{width:'10px',height:'10px'}}/>
+                <S.StarText>304</S.StarText>
+              </div>
+            </S.ColFlexItem>
+          </S.ColFlexContainer>
+        </div>
+          
+      </S.FlexContainer>
     </>
   )
 }
---
+++ src/components/community/NeighborRecipe.style.js
@@ -0,0 +1,102 @@
+import { styled } from "styled-components";
+
+
+
+export const FlexContainer = styled.div`
+  padding-left:30px;
+  padding-top:20px;
+  padding-bottom:86px;
+  display:flex;
+  flex-direction:column;
+  gap:24px;
+`;
+export const FlexItem = styled.div`
+  display:flex;
+  flex-direction:column;
+  gap:14px;
+`;
+export const TitleText = styled.div`
+  ${({theme})=>theme.fonts.pre_semibold_20};
+  line-height: 24px; 
+`;
+export const TextFlex= styled.div`
+  display:flex;
+  align-items:flex-end;
+  gap:6px;
+`;
+export const SubText = styled.div`
+  color: #888;
+  font-family: Pretendard;
+  font-size: 14px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: normal;
+`;
+export const SubText2 = styled.div`
+  color: #888;
+  ${({theme})=>theme.fonts.pre_medium_11};
+  line-height: 24px;
+  text-decoration-line: underline;
+  /* text-decoration-style: solid;
+  text-decoration-skip-ink: auto;
+  text-decoration-thickness: auto;
+  text-underline-offset: auto;
+  text-underline-position: from-font; */
+`;
+
+export const CardWrapper = styled.div`
+  display:flex;
+  gap:10px;
+  overflow-x:auto;
+
+  &::-webkit-scrollbar {
+    display: none;
+  }
+`;
+export const CardContainer = styled.div`
+  display:flex;
+  flex-direction:column;
+  gap:6px;
+`;
+export const CardImage = styled.img`
+  width: ${props => props.$width}px;
+  height: ${props => props.$height}px;
+  border-radius: 12px;
+`;
+export const CardText = styled.div`
+  font-family: Pretendard;
+  ${({theme})=>theme.fonts.pre_medium_14};
+  line-height: 24px;
+  text-align:center;
+`;
+
+export const ColFlexContainer = styled.div`
+  padding-right:25px;
+`; 
+export const ColFlexItem = styled.div`
+  padding:11px 0 14px;
+  display:flex;
+  align-items:flex-end;
+  gap:16px;
+  /* height:100px; */
+  border-bottom: #E6E6E6 solid 1px;
+`;
+export const RoundImg = styled.img`
+  width: 73px;
+  height: 73px;
+  border-radius:73px;
+`;
+export const NickText = styled.div`
+  color: #888;
+  ${({theme})=>theme.fonts.pre_medium_11};
+  line-height: 24px;
+`;
+export const RecipeTitle = styled.div`
+  ${({theme})=>theme.fonts.pre_medium_14};
+  line-height: 20px;
+`;
+export const StarText = styled.span`
+  ${({theme})=>theme.fonts.pre_medium_11};
+  line-height: 24px; 
+  margin-left:4px;
+`;
