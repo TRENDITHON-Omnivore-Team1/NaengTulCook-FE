bb35d5f01458193f4abe1995645314c9831e8896
seunghyeonKang <number42isuser@gmail.com>
2025-02-23 18:45:01 +0900

    🎨: 사전입력 페이지 - step 5, 6 UI 디자인 추가

--- src/components/sign/pre-info/InfoStepD.jsx
+++ src/components/sign/pre-info/InfoStepD.jsx
@@ -1,4 +1,15 @@
+import { useWatch } from "react-hook-form";
 import * as A from "./InfoStep.style";
+import checkSvg from "@/assets/splash/icon_checked_img.svg";
+import ingredient1Png from "@/assets/splash/pre-info-step6/img_1.png";
+import ingredient2Png from "@/assets/splash/pre-info-step6/img_2.png";
+import ingredient3Png from "@/assets/splash/pre-info-step6/img_3.png";
+import ingredient4Png from "@/assets/splash/pre-info-step6/img_4.png";
+import ingredient5Png from "@/assets/splash/pre-info-step6/img_5.png";
+import ingredient6Png from "@/assets/splash/pre-info-step6/img_6.png";
+import ingredient7Png from "@/assets/splash/pre-info-step6/img_7.png";
+import ingredient8Png from "@/assets/splash/pre-info-step6/img_8.png";
+import ingredient9Png from "@/assets/splash/pre-info-step6/img_9.png";
 
 const options = [
   "곡류",
@@ -11,13 +22,27 @@ const options = [
   "해산물",
   "어패류",
 ];
+const imgList = [
+  ingredient1Png,
+  ingredient2Png,
+  ingredient3Png,
+  ingredient4Png,
+  ingredient5Png,
+  ingredient6Png,
+  ingredient7Png,
+  ingredient8Png,
+  ingredient9Png,
+];
 
-export default function InfoStepD({ register }) {
+export default function InfoStepD({ register, control }) {
+  const selectedValue = useWatch({ control, name: "ingredient" });
+
   return (
-    <>
+    <A.ListWrapper>
       {options.map((option, i) => (
-        <label key={i}>
-          <input
+        <A.InputItemContainer key={i}>
+          {/* input(안보임) */}
+          <A.CheckBoxInput
             type="checkbox"
             value={option}
             {...register("ingredient", {
@@ -29,9 +54,20 @@ export default function InfoStepD({ register }) {
               },
             })}
           />
-          {option}
-        </label>
+
+          {/* 사진 */}
+          <A.Container88>
+            <A.InputItemImg src={imgList[i]} />
+            {selectedValue?.includes(option) && (
+              <A.CheckOverlay>
+                <A.CheckIcon src={checkSvg} alt="선택됨" />
+              </A.CheckOverlay>
+            )}
+          </A.Container88>
+          {/* 재료 이름 */}
+          <span>{option}</span>
+        </A.InputItemContainer>
       ))}
-    </>
+    </A.ListWrapper>
   );
 }
