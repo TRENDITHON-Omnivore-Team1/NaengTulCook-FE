c33ea8ae7816c0023b014ee4cc8115ce7ef2b561
seunghyeonKang <number42isuser@gmail.com>
2025-02-25 15:18:21 +0900

    🎨: 공통 상단바 컴포넌트 뒤로가기 버튼 추가 및 이웃레시피 등록 레이아웃 수정

--- src/components/community/write-neighbor-recipe/WriteRecipeLayout.jsx
+++ src/components/community/write-neighbor-recipe/WriteRecipeLayout.jsx
@@ -1,9 +1,11 @@
+import Topbar from "@/components/common/topbar/Topbar";
 import RecipeStepA from "./RecipeStepA";
 import * as A from "./WriteRecipeLayout.style";
 
 export default function WriteRecipeLayout() {
   return (
     <>
+      <Topbar pageTitle="레시피 공유" isSubPage={true} />
       <RecipeStepA />
     </>
   );
